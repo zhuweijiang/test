544759a75a7521e1accd15ede891806e6d74cae0
zhuweijiang <zhuweijiang_1987@126.com>
2017-07-25 18:09:46 +0800

    测试

---
+++ hellojs.js
@@ -0,0 +1,16 @@
+function hello() {
+    alert('你点了hahhh');
+}
+var str1 = "2";
+var str2 = [1, 2];
+var obj = {
+    "key1": 4,
+    "key2": false,
+    "key3": function () {
+        alert('你点了hello');
+    },
+    "key4": str1,
+    "key5": str2
+
+}
+console.log(obj);
